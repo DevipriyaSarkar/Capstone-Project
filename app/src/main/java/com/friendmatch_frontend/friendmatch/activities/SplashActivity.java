f2716eb2b7eef342a9e561be48cd5db45d650c9f
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2017-01-13 02:33:07 +0530

    chore: remove local host option
    not relevant for capstone project - this project uses only pythonanywhere server

--- app/src/main/java/com/friendmatch_frontend/friendmatch/activities/SplashActivity.java
+++ app/src/main/java/com/friendmatch_frontend/friendmatch/activities/SplashActivity.java
@@ -7,14 +7,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.friendmatch_frontend.friendmatch.R;
 
-import static com.friendmatch_frontend.friendmatch.application.AppController.LOCAL_IP_ADDRESS;
-import static com.friendmatch_frontend.friendmatch.application.AppController.MY_SERVER;
 import static com.friendmatch_frontend.friendmatch.application.AppController.SERVER_URL;
 
 public class SplashActivity extends AppCompatActivity {
@@ -42,62 +37,13 @@ public class SplashActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     SharedPreferences.Editor e = sp.edit();
-                    e.putString("SERVER_URL", MY_SERVER);
+                    e.putString("SERVER_URL", SERVER_URL);
                     e.apply();
-                    SERVER_URL = MY_SERVER;
                     finish();
                     Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(intent);
                 }
             });
-
-            Button enableLocalHostButton = (Button) findViewById(R.id.enableLocalHostButton);
-            enableLocalHostButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startButton.setVisibility(View.GONE);
-                    LinearLayout localHostLayout = (LinearLayout) findViewById(R.id.localHostLayout);
-                    localHostLayout.setVisibility(View.VISIBLE);
-
-                    final EditText localHostAddress = (EditText) findViewById(R.id.localHostAddress);
-
-                    Button localHostStartButton = (Button) findViewById(R.id.localHostStartButton);
-                    localHostStartButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            String address = localHostAddress.getText().toString();
-                            if (isEmpty(address)) {
-                                Toast.makeText(getApplicationContext(), R.string.local_host_address_empty_error, Toast.LENGTH_SHORT).show();
-                            } else if (isInvalid(address)) {
-                                Toast.makeText(getApplicationContext(), R.string.local_host_address_incorrect_error, Toast.LENGTH_SHORT).show();
-                            } else {
-                                LOCAL_IP_ADDRESS = "http://" + address;
-                                SharedPreferences.Editor e = sp.edit();
-                                e.putString("SERVER_URL", LOCAL_IP_ADDRESS);
-                                e.apply();
-                                SERVER_URL = LOCAL_IP_ADDRESS;
-                                finish();
-                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                                startActivity(intent);
-                            }
-                        }
-                    });
-                }
-            });
-
-        } else {
-            SERVER_URL = sp.getString("SERVER_URL", MY_SERVER);
-            finish();
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(intent);
         }
-    }
-
-    private boolean isEmpty(String string) {
-        return string == null || string.equals("") || string.equals(" ");
-    }
-
-    private boolean isInvalid(String string) {
-        return !string.matches(".+[.].+[.].+[.].+");
     }
 }
